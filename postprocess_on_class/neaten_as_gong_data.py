e8623b1a8b614b5ac77aa77a025b80471487dc4e
Wenger-In <97772906+Wenger-In@users.noreply.github.com>
2023-11-28 21:02:28 +0800

    neaten

--- postprocess_on_class/neaten_as_gong_data.py
+++ postprocess_on_class/neaten_as_gong_data.py
@@ -5,36 +5,35 @@ from astropy.io import fits
 
 path = 'E:/Research/Program/SynopticMapPrediction/postprocess_on_class/'
 
-# raw GONG fits: flipud to get synoptic maps
-raw_file = path + 'raw/' + 'mrzqs_c2048.fits'
-# gong_file = path + 'neaten/' + 'neaten_cr2259.fits'
-raw_fits = fits.open(raw_file)
+for cr in range(2278,2279):
+    # raw GONG fits: flipud to get synoptic maps
+    raw_file = path + 'raw/' + 'mrzqs_c'+ str(cr) + '.fits'
+    # raw_file = path + 'neaten/' + 'cr2259_neaten.fits'
+    raw_fits = fits.open(raw_file)
 
-raw_Br = raw_fits[0].data
-raw_header = raw_fits[0].header
-print(raw_header)
+    raw_Br = raw_fits[0].data
+    raw_header = raw_fits[0].header
+    # print(raw_header)
 
-plt.figure()
-plt.imshow(raw_Br,cmap='RdBu',vmin=-50,vmax=50)
-plt.colorbar()
+    plt.figure()
+    plt.imshow(raw_Br,cmap='RdBu',vmin=-50,vmax=50)
+    plt.colorbar()
 
-# predicted WSO mat: flipud to get synoptic maps
-pred_file = path + 'cr2259_interp.mat'
-pred_mat = scio.loadmat(pred_file)
-pred_Br = pred_mat['pred_Br_interp']
+    # predicted WSO mat: flipud to get synoptic maps
+    pred_file = path + 'interp/' + 'cr' + str(cr) + '_interp.mat'
+    pred_mat = scio.loadmat(pred_file)
+    pred_Br = pred_mat['pred_Br_interp']
 
-plt.figure()
-plt.imshow(pred_Br,cmap='RdBu',vmin=-4,vmax=4)
-plt.colorbar()
+    plt.figure()
+    plt.imshow(pred_Br,cmap='RdBu',vmin=-4,vmax=4)
+    plt.colorbar()
 
-# replace original GONG data with predicted data
-gong_fits[0].data = pred_Br
-new_Br = gong_fits[0].data
+    # replace original GONG data with predicted data
+    neaten_fits = raw_fits
+    neaten_fits[0].data = pred_Br
 
-plt.figure()
-plt.imshow(new_Br,cmap='RdBu',vmin=-4,vmax=4)
-plt.colorbar()
+    plt.show()
+    # plt.close()
 
-plt.show()
-
-# gong_fits.writeto(path + 'neaten_cr2259.fits')
+    save_file = path + 'neaten/' + 'cr' + str(cr) + '_neaten.fits'
+    # neaten_fits.writeto(save_file)
